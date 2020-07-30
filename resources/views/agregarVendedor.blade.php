eb3da8223c4a20813181a83bd697519434adf2f1
mesuppes <mesuppes22@gmail.com>
2020-07-29 23:15:05 -0300

    datos de vnededor actualizado

--- resources/views/agregarVendedor.blade.php
+++ resources/views/agregarVendedor.blade.php
@@ -52,10 +52,34 @@
                                                     <input type="mail" name="email" class="form-control" placeholder="Ingrese un mail" value="{{old('email')}}">
                                                 </div>
                                             </div>
+                                            </div>
+                                            <div class="row">
                                             <div class="col-md-6">
                                                 <div class="form-group">
-                                                    <label>CUIT</label>
-                                                    <input type="text" name="cuit" class="form-control" placeholder="Ingrese el CUIT" value="{{old('cuit')}}">
+                                                    <label>CUIT/CUIL</label>
+                                                    <div class="input-group">
+                                                    <div class="input-group-append">
+                                                          <select class="selectpicker form-control" data-style="btn btn-danger btn-block" required name="tipoDocumento">
+                                                            <option selected>Seleccione</option>
+                                                            <option value="CUIT">CUIT</option>
+                                                            <option value="CUIL">CUIL</option>
+                                                            </select>
+                                                    </div>
+                                                    <input type="text" name="cuit" class="form-control" placeholder="Ingrese el nro" value="{{old('cuit')}}">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <div class="form-group">
+                                                    <label>Inscripción social</label>
+                                                    <div class="input-group">
+                                                          <select class="selectpicker form-control" data-style="btn btn-danger btn-block" required name="inscripcionAfip">
+                                                            <option selected>Seleccione</option>
+                                                               <option value="M">Monotributo</option>
+                                                            <option value="RI">Responsable inscripto</option>
+                                                            <option value="CF">Consumidor final</option>
+                                                            </select>
+                                                    </div>
                                                 </div>
                                             </div>
                                             </div>
@@ -76,6 +100,12 @@
                                             <div class="row">
                                             <div class="col-md-6">
                                                 <div class="form-group">
+                                                    <label>Codigo postal</label>
+                                                    <input type="number" name="codigoPostal" class="form-control" placeholder="Ingrese el CP" value="{{old('codigoPostal')}}">
+                                                </div>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <div class="form-group">
                                                     <label>Provincia</label>
                                                     <input type="text" name="provincia" class="form-control" placeholder="Ingrese la provincia" value="{{old('provincia')}}">
                                                 </div>
@@ -85,11 +115,10 @@
                      <div class="bg-white card">
                         <div class="d-inline-flex justify-content-between">
                             <div class="d-flex pr-2">
-                                <button type="submit" id="botonAgregarVendedor" class="btn btn-success">Agregar Vendedor
-</button>
+                                <button type="submit" id="botonAgregarVendedor" class="btn btn-success">Agregar Vendedor</button>
                         </div>
-                             </div>
                          </form>
+                             </div>
                     </div>
                     @if($errors->any())
     <ul>
@@ -101,22 +130,16 @@
                             </div>
                         </div>
 
-</div>
-                            </div>
-
     <script src="{{asset('dashboard/assets/js/core/jquery.min.js')}}"></script>
   <script src="{{asset('dashboard/assets/js/core/popper.min.js')}}"></script>
   <script src="{{asset('dashboard/assets/js/core/bootstrap.min.js')}}"></script>
   <script src="{{asset('dashboard/assets/js/plugins/perfect-scrollbar.jquery.min.js')}}"></script>
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
-
-    <script type="text/javascript">
-
-
-</script>
+        <!-- include footer -->
+        @include('layouts.partials.footer')
+    </div>
 </div>
-</div>
-
+@endsection
 
 
 
