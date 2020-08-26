2019d6b5cfad741041c2ea2903590947d6f54871
Nicolas Marcos De Lucca <nicolasmarcos.dx@gmail.com>
2020-08-26 17:07:30 -0300

    varios

--- app/ProductoStockLote.php
+++ app/ProductoStockLote.php
@@ -8,4 +8,9 @@ class ProductoStockLote extends Model
 {
     protected $table ='v_productos_stock_lotes';
     protected $primaryKey ='id_producto';
+
+	public function producto(){
+        return $this->belongsTo(Producto::class,'id_producto','id_producto');
+    }
+
 }
